0dad5f6ccf81c9317dafada08a380e10bc7d0313
mjkeefe <matthew.j.keefe@gmail.com>
2018-01-28 18:04:58 -0500

    added BSTS model fit with predictions

--- Florida Gas Price Modeling.R
+++ Florida Gas Price Modeling.R
@@ -2,8 +2,10 @@
 # September 10, 2017
 # Predicting Weekly Florida Gas Prices
 
+library(bsts)
+
 #set working directory
-setwd("C:/Users/Matthew/Dropbox/Matts Stuff/Florida Gas Prices")
+setwd("C:/Users/Matthew/Dropbox/Repositories/gas-forecasting")
 
 #read in and format data
 FLgas<-read.csv("FLgas.csv",header=FALSE)
@@ -13,4 +15,17 @@ FLgas$date<-as.Date(FLgas$date,format="%Y-%m-%d")
 #plot time series of FL gas prices
 plot(FLgas$date,FLgas$price,type='l',xlab="Date",ylab="Price per Gallon")
 
+#split data into test and train
+numweeks<-10
+train <- FLgas[1:(nrow(FLgas)-(numweeks)),]
+test <- FLgas[(nrow(FLgas)-(numweeks-1)):nrow(FLgas),]
 
+#try a model
+ss <- list()
+ss <- AddLocalLevel(ss, train$price)
+set.seed(2181)
+model <- bsts(price~., state.specification = ss, niter = 10000, data = train)
+
+#predict for numweeks
+model.preds <- predict(model, newdata=test, horizon = nrow(test), burn = 1000)
+plot(model.preds, plot.original = 25)
