4c6b9c58429cbfe45d023437ca4436bc3465c647
mjkeefe <matthew.j.keefe@gmail.com>
2018-02-04 16:52:11 -0500

    added validation plot and more componenets to bsts model

--- Florida Gas Price Modeling.R
+++ Florida Gas Price Modeling.R
@@ -22,10 +22,14 @@ test <- FLgas[(nrow(FLgas)-(numweeks-1)):nrow(FLgas),]
 
 #try a model
 ss <- list()
-ss <- AddLocalLevel(ss, train$price)
+#ss <- AddLocalLevel(ss, train$price)
+ss <- AddLocalLinearTrend(ss, train$price)
+ss <- AddSeasonal(ss, train$price, nseasons = 52)
 set.seed(2181)
 model <- bsts(price~., state.specification = ss, niter = 10000, data = train)
 
 #predict for numweeks
 model.preds <- predict(model, newdata=test, horizon = nrow(test), burn = 1000)
+plot.bsts(model, "components")
 plot(model.preds, plot.original = 25)
+points(x = (nrow(FLgas)-(numweeks-1)):nrow(FLgas), y = test$price , col = "red", pch = 16)
